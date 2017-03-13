91a3b8da4b434f29a3e862c7e82a4020bb21b7c5
千涯秋瑟 <1476265623@qq.com>
2017-03-14 00:03:33 +0800

    初始化

--- HelloScript/02/es.js
+++ HelloScript/02/es.js
@@ -1,3 +1,7 @@
 /**
  * Created by molu_ on 2017/3/12.
  */
+
+let result=[1,2,3].map(x=>x*4);
+
+console.log(result);
---
+++ HelloScript/dist/02/dist/es.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/**
+ * Created by molu_ on 2017/3/12.
+ */
+
+var result = [1, 2, 3].map(function (x) {
+  return x * 4;
+});
+
+console.log(result);
+//# sourceMappingURL=es.js.map
